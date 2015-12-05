c4f0627f6bcd2742c9a8d4484143672520565825
Patrick Brandão <patrickbrandao@gmail.com>
2015-12-04 23:09:56 -0200

    Create version.h

---
+++ version.h
@@ -0,0 +1,10 @@
+/*
+	LIB: definicoes para terminfo
+	AUTOR: Marcio Jode de Araujo
+	DATA: 30/06/2012
+*/
+
+#define author "Marcio Araujo <contato@mysab.com.br>, Patrick Brandao <contato@tmsoft.com.br>"
+
+#define subversion 1
+#define version 1
